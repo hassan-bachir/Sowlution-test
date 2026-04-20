bc331feea643afb7b54956a2b31c8542e8145fb9
Hassan Bachir <125279202+hassan-bachir@users.noreply.github.com>
2026-04-20 14:14:02 +0300

    Add initial implementation of game logic and data processing
    - Created `prediction.csv` with game data.
    - Implemented `smallestDivisibleNumber` function in `problem1.js`.
    - Added data reading and processing logic in `problem2.js`.
    - Defined probability calculation for game outcomes in `problem2.js`.
    - Created empty files for future implementation in `problem3.js` and `problem4.js`.

---
+++ problem1.js
@@ -0,0 +1,20 @@
+function smallestDivisibleNumber(n) {
+  let result = n;
+  if (!Number.isInteger(n)) {
+    return "Input is not Ineteger";
+  }
+  while (true) {
+    let flag = true;
+    for (let i = 1; i <= n; i++) {
+      if (result % i !== 0) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      return result;
+    }
+    result++;
+  }
+}
+console.log(smallestDivisibleNumber(10));
---
+++ problem2.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+
+const rawData = fs.readFileSync("prediction.csv", "utf-8");
+// console.log(rawData);
+const dataArray = rawData.split(/\r?\n/);
+// console.log(data);
+// const header = dataArray[0].split(",");
+// console.log(header)
+let rows = dataArray.slice(1);
+// console.log(rows);
+const data = rows.map((row) => {
+  const values = row.split(",");
+  const obj = {
+    suit: values[0],
+    animal: values[1],
+    fruit: values[2],
+    won: Number(values[3] == "True"),
+  };
+
+  return obj;
+});
+// this one is the empty last slot NOT NEEDED
+const emptyLastEntry = data.pop();
+// console.log(data);
+
+const totalWon = data.filter((entry) => entry.won === 1).length;
+const totalLost = data.filter((entry) => entry.won === 0).length;
+const totalplayers = data.length;
+const winRate = totalWon / totalplayers;
+const lossRate = totalLost / totalplayers;
+
+// console.log(totalLost);
+// console.log(totalWon);
+// console.log(totalplayers);
+// console.log(winRate);
+// console.log(lossRate);
+const checkCardType = (str) => {
+  const allowedValues = ["diamonds", "spades", "hearts", "joker", "clubs"];
+  return allowedValues.includes(str.toLowerCase());
+};
+const checkAnimalType = (str) => {
+  const allowedValues = ["lion", "parrot", "fox", "seal", "snake"];
+  return allowedValues.includes(str.toLowerCase());
+};
+const checkFruitType = (str) => {
+  const allowedValues = ["apple", "banana", "mango", "papaya", "watermelon"];
+  return allowedValues.includes(str.toLowerCase());
+};
+
+function probabilityToBeatBoss(targetSuit, targetAnimal, targetFruit) {
+  if (
+    !checkCardType(targetSuit) ||
+    !checkAnimalType(targetAnimal) ||
+    !checkFruitType(targetFruit)
+  ) {
+    return "Invalid Input";
+  }
+  const winners = data.filter((entry) => entry.won === 1);
+  const losers = data.filter((entry) => entry.won === 0);
+
+  const suitWinRate =
+    winners.filter((entry) => entry.suit === targetSuit).length / totalWon;
+  const animalWinRate =
+    winners.filter((entry) => entry.animal === targetAnimal).length / totalWon;
+  const fruitWinRate =
+    winners.filter((entry) => entry.fruit === targetFruit).length / totalWon;
+
+  const winScore = suitWinRate * animalWinRate * fruitWinRate * winRate;
+  //   console.log(winScore);
+
+  const suitLossRate =
+    losers.filter((entry) => entry.suit === targetSuit).length / totalLost;
+  const animalLossRate =
+    losers.filter((entry) => entry.animal === targetAnimal).length / totalLost;
+  const fruitLossRate =
+    losers.filter((entry) => entry.fruit === targetFruit).length / totalLost;
+
+  const lossScore = suitLossRate * animalLossRate * fruitLossRate * lossRate;
+
+  const probability = (winScore / (winScore + lossScore)) * 100;
+
+  return probability.toFixed(2) + "%";
+}
+
+const result = probabilityToBeatBoss("Hearts", "Snake", "Mango");
+console.log(`${result}`);
